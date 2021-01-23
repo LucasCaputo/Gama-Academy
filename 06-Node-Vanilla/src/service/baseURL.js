e9bfe2c92df0912eab180f6b96d8cc9f36450b07
Caputo <lccaputo@hotmail.com>
2021-01-23 19:13:23 -0300

    feat: controle de rotas com JS vanilla

---
+++ 06-Node-Vanilla/src/service/baseURL.js
@@ -0,0 +1,5 @@
+
+const baseURL = "https://accenture-java-desafio.herokuapp.com/"
+
+// const baseURL = "accenture-java-desafio.herokuapp.com/"
+export default baseURL;
---
+++ 06-Node-Vanilla/src/views/components/footer.js
@@ -0,0 +1,12 @@
+let Footer = {
+  render: () => {
+    let view = `<footer class="my-5 pt-5 text-muted text-center text-small">
+                  <p>Lucas Caputo</p>
+                </footer>`;
+
+    return view;
+  },
+  after_render: () => {},
+};
+
+export default Footer;
